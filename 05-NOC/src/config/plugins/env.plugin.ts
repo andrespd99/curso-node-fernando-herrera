b43810c6a9cbc679cea0c5f9c16389fdc6c40495
Andres Eloy <andres.epacheco99@gmail.com>
2024-02-18 16:02:41 -0400

    feat: ENV variables with dotenv and env-var packages

---
+++ 05-NOC/src/config/plugins/env.plugin.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+import * as _env from 'env-var';
+
+
+export const env = {
+    PORT: _env.get('PORT').required().asPortNumber(),
+    MAILER_EMAIL: _env.get('MAILER_EMAIL').required().asEmailString(),
+    MAILER_SECRET_KEY: _env.get('MAILER_SECRET_KEY').required().asString(),
+    PROD: _env.get('PROD').required().asBool(),
+}
